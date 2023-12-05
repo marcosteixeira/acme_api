2d363b3a9ab46584dee12b5e554d77cf2cdc364c
Marcos Teixeira <m.viniteixeira@gmail.com>
2023-12-05 17:19:59 +0000

    Fix application controller by changing from before_filter to before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  before_filter :user_quota
+  before_action :user_quota
 
   def user_quota
     Hit.transaction do
       Hit.create(user: current_user, endpoint: request.path)
       render json: { error: 'over quota' } if current_user.count_hits >= 10_000
     end
+  end
+
+  def current_user
+    # omitting authentication for simplicity
+    raise NotImplementedError
   end
 end
