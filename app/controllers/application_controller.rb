424459a6b61721a4757ad1bd29e06d960f7e3356
Marcos Teixeira <m.viniteixeira@gmail.com>
2023-12-05 14:06:02 +0000

    Add Rubocop for code lint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
-class ApplicationController < ActionController::API
-	before_filter :user_quota
+# frozen_string_literal: true
 
-	def user_quota
+class ApplicationController < ActionController::API
+  before_filter :user_quota
+
+  def user_quota
     Hit.transaction do
       Hit.create(user: current_user, endpoint: request.path)
-      render json: { error: 'over quota' } if current_user.count_hits >= 10000
+      render json: { error: 'over quota' } if current_user.count_hits >= 10_000
     end
   end
 end
