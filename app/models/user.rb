bd0e6f107e409fef9699fc2732a6cd7028e62b44
Marcos Teixeira <m.viniteixeira@gmail.com>
2023-12-05 13:02:32 +0000

    Add timezone to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 	has_many :hits
+  validates :timezone, inclusion: { in: ActiveSupport::TimeZone.all.map { |tz| tz.tzinfo.name } },
+    allow_blank: true
 
 	def count_hits
 		start = Time.now.beginning_of_month
